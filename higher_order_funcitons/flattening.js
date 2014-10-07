c3c41ca0905543e8fbabb8c0bf5e1466e9602640
Radimir Bitsov <rbitsov@insitex.com>
2014-10-07 11:34:04 +0300

    Complete exercise 2 from higher-order functions chapter.

--- higher_order_funcitons/flattening.js
+++ higher_order_funcitons/flattening.js
@@ -8,8 +8,9 @@ function flattenArray(multiArray) {
   });
 }
 
+console.log("---------------------");
+
 console.log(flattenArray([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
 // -> [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-var ancestry = JSON.parse(ANCESTRY_FILE);
-console.log(ancestry.length);
+console.log("---------------------");
