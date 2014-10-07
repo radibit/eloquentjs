68b330d580a798e4e604ea235d7d2c0e8e917bf0
Radimir Bitsov <rbitsov@insitex.com>
2014-10-07 11:31:56 +0300

    Complete exercise 2 from higher-order functions chapter.

---
+++ higher_order_funcitons/motherChildAgeDiff.js
@@ -0,0 +1,27 @@
+/**
+ * Created by rbit on 10/6/14.
+ */
+
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+var ancestry = JSON.parse(ANCESTRY_FILE);
+
+var byName = {};
+ancestry.forEach(function(person) {
+  byName[person.name] = person;
+});
+
+function mother(el) {
+  return byName[el.mother] !== undefined;
+}
+
+function ageDiff(el) {
+  return el.born - byName[el.mother].born;
+}
+
+console.log('Average age difference between mothers and children is ' + average(ancestry.filter(mother).map(ageDiff)));
+
+console.log("---------------------");
