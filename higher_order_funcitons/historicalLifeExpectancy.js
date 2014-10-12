102076fbd0afc4e5766df80ea3c5d707579181c5
Radimir Bitsov <rbitsov@insitex.com>
2014-10-13 01:15:24 +0300

    Complete exercise 3, chapter 5.

---
+++ higher_order_funcitons/historicalLifeExpectancy.js
@@ -0,0 +1,29 @@
+/**
+ * Created by rbit on 10/7/14.
+ */
+
+var ancestry = JSON.parse(ANCESTRY_FILE),
+    group = {};
+
+function average(array) {
+  function plus(a, b) { return a + b; }
+  return array.reduce(plus) / array.length;
+}
+
+ancestry.forEach(function(person) {
+  var currentCentury = Math.ceil(person.died / 100);
+  if (!group[currentCentury]) {
+    group[currentCentury] = [];
+  }
+  group[currentCentury].push(person.died - person.born);
+});
+
+for (var century in group) {
+  if (group.hasOwnProperty(century)) {
+    console.log(century + ': ' + average(group[century]).toFixed(1));
+  }
+}
+
+//console.log(group);
+
+console.log("---------------------");
